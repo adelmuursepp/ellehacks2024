fc03dce9d9c82b826ec6e2da885c4cda9faae9b9
Billie Zhang <billie.zhang1504@gmail.com>
2024-02-17 18:51:44 -0500

    update navbar logo link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,21 @@
 import logo from "../assets/Seasonal.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="w-full h-32 flex fixed ">
-      <img
-        src={logo}
-        alt="logo"
-        style={{
-          height: "40px",
-          marginLeft: "20px",
-          marginTop: "20px",
-          paddingBottom: "15px",
-        }}
-      />
+      <Link to="/">
+        <img
+          src={logo}
+          alt="logo"
+          style={{
+            height: "40px",
+            marginLeft: "20px",
+            marginTop: "20px",
+            paddingBottom: "15px",
+          }}
+        />
+      </Link>
     </div>
   );
 };
