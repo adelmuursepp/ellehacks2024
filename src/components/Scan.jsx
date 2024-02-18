5b10167de9f8319995c888708c3f24df291229af
Billie Zhang <billie.zhang1504@gmail.com>
2024-02-17 22:02:48 -0500

    add ripeness check

--- src/components/Scan.jsx
+++ src/components/Scan.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Strawberry from "../assets/strawberry.jpeg";
+import FruitRipenessChecker from "./RipenessCheck";
 
 const Scan = () => {
   return (
     <div>
       <div>
         <h2>scan camera</h2>
+        <FruitRipenessChecker />
 
         <h3>name of fruit</h3>
       </div>
