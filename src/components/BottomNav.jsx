52921c6783a8d39ef23536558ab5dbe1b00777da
Billie Zhang <billie.zhang1504@gmail.com>
2024-02-17 21:25:26 -0500

    update scan and some testing

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -9,40 +9,43 @@ import PersonIcon from "@mui/icons-material/Person";
 import { Outlet, Link } from "react-router-dom";
 
 export default function BottomNav() {
-  const [value, setValue] = React.useState("home");
+  const [value, setValue] = React.useState("scan");
 
+  // eslint-disable-next-line
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      sx={{
-        "& .Mui-selected, .Mui-selected > svg": {
-          color: "#007A78",
-        },
-      }}
-    >
-      <Link to="/">
-        <BottomNavigationAction value="home" icon={<HomeIcon />} />
-      </Link>
-      <Link to="/search">
-        <BottomNavigationAction value="search" icon={<SearchIcon />} />
-      </Link>
-      <Link to="/scan">
-        <BottomNavigationAction value="scan" icon={<PhotoCameraIcon />} />
-      </Link>
-      <Link to="/recipes">
-        <BottomNavigationAction value="recipes" icon={<MenuBookIcon />} />
-      </Link>
-      <Link to="/profile">
-        <BottomNavigationAction value="profile" icon={<PersonIcon />} />
-      </Link>
-      <Outlet />
-    </BottomNavigation>
+    <div className="fixed bottom-0 w-full z-50">
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        sx={{
+          "& .Mui-selected, .Mui-selected > svg": {
+            color: "#007A78",
+          },
+        }}
+      >
+        <Link to="/">
+          <BottomNavigationAction value="home" icon={<HomeIcon />} />
+        </Link>
+        <Link to="/search">
+          <BottomNavigationAction value="search" icon={<SearchIcon />} />
+        </Link>
+        <Link to="/scan">
+          <BottomNavigationAction value="scan" icon={<PhotoCameraIcon />} />
+        </Link>
+        <Link to="/recipes">
+          <BottomNavigationAction value="recipes" icon={<MenuBookIcon />} />
+        </Link>
+        <Link to="/profile">
+          <BottomNavigationAction value="profile" icon={<PersonIcon />} />
+        </Link>
+        <Outlet />
+      </BottomNavigation>
+    </div>
   );
 }
--- src/components/Scan.jsx
+++ src/components/Scan.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Strawberry from "../assets/strawberry.jpeg";
 
 const Scan = () => {
-  return <div>Scan</div>;
+  return (
+    <div>
+      <div>
+        <h2>scan camera</h2>
+
+        <h3>name of fruit</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Scan;
